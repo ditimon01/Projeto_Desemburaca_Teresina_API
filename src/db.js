e59007ab8bb81322aa3d58bc2fac1c25d711ad11
Vinicíus <fonsecavinicius12@gmail.com>
2025-06-10 12:08:27 -0300

    ajuste para apenas a senha ser variável de ambiente

--- src/db.js
+++ src/db.js
@@ -1,5 +1,14 @@
 const { Pool } = require('pg');
 
-const pool = new Pool(process.env.DB_POOL)
+const pool = new Pool({
+    user: 'postgres',
+    host: 'db.hvezapxfmgzmhppqllmi.supabase.co',
+    database: 'postgres',
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 module.exports = pool;
