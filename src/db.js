6664e71c16217c7507adc89c9ea7f4ccf18bf565
Vinicíus <fonsecavinicius12@gmail.com>
2025-06-10 11:52:03 -0300

    ajustes para utilização de variáveis de ambiente

--- src/db.js
+++ src/db.js
@@ -1,14 +1,5 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'db.hvezapxfmgzmhppqllmi.supabase.co',
-    database: 'postgres',
-    password: '10126824!euamopostgis',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+const pool = new Pool(process.env.DB_POOL)
 
 module.exports = pool;
